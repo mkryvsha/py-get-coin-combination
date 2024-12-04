7a5ab6028a5e06428789c7a8c50b1a5a744878b1
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-12-04 13:20:15 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 def get_coin_combination(cents: int) -> list:
-    values = [1, 5, 10, 25]
-    coins = [0, 0, 0, 0]
+    if cents >= 0:
+        values = [1, 5, 10, 25]
+        coins = [0, 0, 0, 0]
 
-    for i in range(3, -1, -1):
-        coins[i] = cents // values[i]
-        cents -= coins[i] * values[i]
+        for i in range(3, -1, -1):
+            coins[i] = cents // values[i]
+            cents -= coins[i] * values[i]
 
-    return coins
+        return coins
+
+    else:
+        raise ValueError
--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,27 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestClass:
+
+    @pytest.mark.parametrize(
+        "cents, result",
+        [
+            pytest.param(1, [1, 0, 0, 0], id="1 penny"),
+            pytest.param(6, [1, 1, 0, 0],
+                         id="1 penny + 1 nickel"),
+            pytest.param(17, [2, 1, 1, 0],
+                         id="2 pennies + 1 nickel + 1 dime"),
+            pytest.param(50, [0, 0, 0, 2], id="2 quarters"),
+        ]
+    )
+    def test_that_should_check_all_sheet(
+            self,
+            cents: int,
+            result: list[int]) -> None:
+        actual_result = get_coin_combination(cents)
+        assert actual_result == result
+
+    def test_should_try_on_errors(self) -> None:
+        with pytest.raises(ValueError):
+            get_coin_combination(-1)
